c50b160727e73dad895b8010775e5682437a8910
Qamar-Zaman <qamarzaman123pt@gmail.com>
2025-02-05 05:44:00 -0800

    pro-web

--- next.config.ts
+++ next.config.ts
@@ -6,13 +6,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
-  // Your existing Next.js configuration
-});
 
