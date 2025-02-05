d263b8416e8332d007a527f86198c95fb5441934
Qamar-Zaman <qamarzaman123pt@gmail.com>
2025-02-05 07:28:56 -0800

    pro-web

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+ const nextconfig= {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 }
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+import { NextConfig } from 'next';
 
-module.exports = withBundleAnalyzer({
-  // Other Next.js configurations...
-});
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
 
+export default nextConfig;
 
 
