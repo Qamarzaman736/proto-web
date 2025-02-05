d99ebad5e9fe1772da150a5aa3f83e6d937a226a
Qamar-Zaman <qamarzaman123pt@gmail.com>
2025-02-05 05:48:51 -0800

    pro-web

--- next.config.ts
+++ next.config.ts
@@ -5,5 +5,12 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  
+});
 
 
